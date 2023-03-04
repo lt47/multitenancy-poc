2c3e5aa0984528f7d786ea8262312d4aa574054d
lt47 <55928190+lt47@users.noreply.github.com>
2023-03-04 15:51:09 -0600

    attempting multi-tenancy

---
+++ db/migrate/20230304210024_create_organizations.rb
@@ -0,0 +1,11 @@
+class CreateOrganizations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :subdomain
+      t.string :domain
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_210034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "subdomain"
+    t.string "domain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "organization_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_projects_on_organization_id"
+  end
+
+  add_foreign_key "projects", "organizations"
 end
