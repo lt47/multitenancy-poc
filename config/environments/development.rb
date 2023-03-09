c06775f24c12ce1ea11e15566765a1d536d30974
lt47 <55928190+lt47@users.noreply.github.com>
2023-03-09 15:09:39 -0600

    Highlighting what I added to enable the use of subdomains in my local env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,5 +70,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # This allows me to use subdomains in my local environment.
   config.action_dispatch.tld_length = 0
 end
